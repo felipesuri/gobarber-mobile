d5291d124b6af388e42a7e5e5d60b09be5ba09bb
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-06 19:28:14 -0300

    :art: configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  editorFormatOnSave: true,
+  printWidth: 90,
+  arrowParens: 'avoid',
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSpacing: true,
+  useTabs: false,
+};
