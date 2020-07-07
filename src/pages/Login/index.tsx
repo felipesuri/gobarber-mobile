89ae106d211fe6d71e3dc8e3c363affe6ccef3c2
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-07 10:51:55 -0300

    :zap: finish login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -10,16 +11,38 @@ import * as S from './styled'
 
 const Login: React.FC = () => {
   return (
-    <S.LoginWrapper>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <S.LoginWrapper>
+            <Image source={logoImg} />
 
-      <S.Title>Faça seu login</S.Title>
+            <S.Title>Faça seu login</S.Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => console.log('ok')}>Entrar</Button>
-    </S.LoginWrapper>
+            <Button onPress={() => console.log('ok')}>Entrar</Button>
+
+            <S.ForgotPassword onPress={() => console.log('ok')}>
+              <S.ForgotPasswordText>Esqueci minha senha</S.ForgotPasswordText>
+            </S.ForgotPassword>
+          </S.LoginWrapper>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <S.CreateAccountButton onPress={() => console.log('ok')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <S.CreateAccountButtonText>Criar uma conta</S.CreateAccountButtonText>
+      </S.CreateAccountButton>
+    </>
   )
 }
 
