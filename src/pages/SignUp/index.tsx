8d38ba00d48f099d3a29b165589caf2c42026ccd
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-08 07:48:39 -0300

    :zap: configure SingUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import logoImg from '../../assets/logo.png'
+
 import * as S from './styled'
 
-const SignUp: React.FC = () => {
-  return <S.SignUpWrapper />
+const SingUp: React.FC = () => {
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <S.SignUpWrapper>
+            <Image source={logoImg} />
+
+            <S.Title>Crie sua conta</S.Title>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => console.log('ok')}>Entrar</Button>
+          </S.SignUpWrapper>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <S.BackButton onPress={() => console.log('ok')}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <S.BackButtonText>Voltar para Login</S.BackButtonText>
+      </S.BackButton>
+    </>
+  )
 }
 
-export default SignUp
+export default SingUp
--- src/pages/SignUp/styled.ts
+++ src/pages/SignUp/styled.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components/native'
+import { Platform } from 'react-native'
 
-export const SignUpWrapper = styled.View``
+export const SignUpWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
+`
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ebe8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+`
+
+export const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 2px;
+  border-color: #232129;
+  padding: 16px 0 16px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const BackButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`
