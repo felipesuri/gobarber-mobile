a61dc8bb8b0fedd2ef7e45229542af183129cb62
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-08 19:12:25 -0300

    :rotating_light: configure errors

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,15 @@
-import React, { useRef } from 'react'
+import React, { useRef, useCallback } from 'react'
 import {
   Image,
   KeyboardAvoidingView,
   Platform,
   ScrollView,
   TextInput,
+  Alert
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import * as Yup from 'yup'
 
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
@@ -19,12 +21,48 @@ import logoImg from '../../assets/logo.png'
 
 import * as S from './styled'
 
+import getValidationErrors from '../../utils/getValidationErrors'
+
+interface SignUpFormData {
+  name: string
+  email: string
+  password: string
+}
+
 const SingUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
   const navigation = useNavigation()
 
   const emailInputRef = useRef<TextInput>(null)
   const passwordInputRef = useRef<TextInput>(null)
+
+  const handleSignUp = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(8, 'Tamanho mínimo: 8 digítos')
+      })
+
+      await schema.validate(data, { abortEarly: false })
+
+      // await api.post('/users', data)
+
+      // history.push('/')
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err)
+
+        formRef.current?.setErrors(errors)
+
+        return
+      }
+
+      Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, tente novamente')
+    }
+  }, [])
 
   return (
     <>
@@ -45,9 +83,7 @@ const SingUp: React.FC = () => {
             <Form
               ref={formRef}
               style={{ width: '100%' }}
-              onSubmit={(data: object) => {
-                console.log(data)
-              }}
+              onSubmit={handleSignUp}
             >
               <Input
                 name="name"
