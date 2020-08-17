f2f400aba5c719941424fdc622a73ba8eaa581bc
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-17 08:18:43 -0300

    :package: push providers from api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import { useAuth } from '../../hooks/auth'
+import api from '../../services/api'
 
 import * as S from './styled'
 
+export interface Providers {
+  id: string
+  name: string
+  avatar_url: string
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Providers[]>([])
+
   const { signOut, user } = useAuth()
   const { navigate } = useNavigation()
+
+  useEffect(() => {
+    api.get('/providers').then(response => {
+      setProviders(response.data)
+    })
+  }, [])
 
   function handleNavigateToProfile() {
     navigate('Profile')
@@ -26,6 +41,12 @@ const Dashboard: React.FC = () => {
           <S.UserAvatar source={{ uri: user.avatar_url }} />
         </S.ProfileButton>
       </S.Header>
+
+      <S.ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <S.UserName>{item.name}</S.UserName>}
+      />
     </S.DashboardWrapper>
   )
 }
